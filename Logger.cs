f92fd743e1ac1a1cfbd02486c6175ccd918dd2b2
Katrina Rosales <katrinarosales@live.com>
2016-02-23 14:39:34 +0100

    Commit övning8

---
+++ Logger.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_8
+{
+    class Logger
+    {
+        // default konstruktor
+        public Logger()
+        {
+            LogPosts = new List<string>();
+        }
+
+        //metod
+
+        public void Log(string message)
+        {
+            // med add så adderas all värde från string message i listan LogPosts
+            LogPosts.Add(message);
+        }        //List property        public List<string> LogPosts { get; private set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //objekt
+            var myLogger = new Logger();
+            myLogger.Log("First post");
+            myLogger.Log("Second post");            //visar hela listan            foreach (var item in myLogger.LogPosts)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
